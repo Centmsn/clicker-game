866a73e40c307d88dd8fd2fafac5d030b712371b
Szadziu <mcj.mikolajczak@gmail.com>
2021-10-30 20:40:45 +0200

    add style to skill emblement

--- src/components/generics/UpgradeCard/UpgradeCard.tsx
+++ src/components/generics/UpgradeCard/UpgradeCard.tsx
@@ -1,18 +1,25 @@
+import SkillEmblement from "../SkillEmblement";
+import { PropsWithChildren } from "react";
 import { UpgradeCardProps } from "./constants";
 import * as P from "./parts";
 
 const UpgradeCard = () => {
-  // generateUpgradesBoxes = () => {
-
-  // }
-
   return (
     <P.Card>
       <P.HeroName>Magnus</P.HeroName>
       <P.HeroLevel>level 27</P.HeroLevel>
       <P.HorizontalLine />
       <P.HeroPortrait />
-      {/* <SkillEmblement/> */}
+      <P.LevelUpSection>
+        levelUp <P.LevelUpButton>🆙</P.LevelUpButton> cost: 12k 🪙
+      </P.LevelUpSection>
+      <P.UpgradesSkillsWrapper>
+        <SkillEmblement />
+        <SkillEmblement />
+        <SkillEmblement />
+        <SkillEmblement />
+        <SkillEmblement />
+      </P.UpgradesSkillsWrapper>
     </P.Card>
   );
 };
--- src/components/generics/UpgradeCard/parts.ts
+++ src/components/generics/UpgradeCard/parts.ts
@@ -1,11 +1,21 @@
 import styled from "styled-components";
 import heroPortrait from "assets/Temporary_Assets/hero.png";
 
+export const LevelUpButton = styled.button`
+  font-size: 40px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 export const Card = styled.div`
   position: relative;
   width: 80%;
   height: 25%;
-  background-color: orange;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  border: solid 4px ${({ theme }) => theme.colors.primary};
+  border-radius: 10px;
 `;
 
 export const HeroName = styled.p`
@@ -37,4 +47,25 @@ export const HeroPortrait = styled.div`
   background-size: contain;
   background-position: center;
   background-repeat: no-repeat;
+`;
+
+export const LevelUpSection = styled.div`
+  position: absolute;
+  top: 30%;
+  left: 40%;
+  margin-top: 10px;
+  font-size: 26px;
+  width: 60%;
+  height: 45%;
+`;
+
+export const UpgradesSkillsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  top: 80%;
+  left: 5%;
+  width: 90%;
+  height: 15%;
+  margin-top: 5px;
 `;
