0e870558b7719d32d7695328d7917b22659d7a6b
Szadziu <mcj.mikolajczak@gmail.com>
2021-10-30 18:16:34 +0200

    add style to Upgrade Card

--- src/components/generics/UpgradeCard/UpgradeCard.tsx
+++ src/components/generics/UpgradeCard/UpgradeCard.tsx
@@ -2,7 +2,19 @@ import { UpgradeCardProps } from "./constants";
 import * as P from "./parts";
 
 const UpgradeCard = () => {
-  return <P.Card>Karta ulepszeń</P.Card>;
+  // generateUpgradesBoxes = () => {
+
+  // }
+
+  return (
+    <P.Card>
+      <P.HeroName>Magnus</P.HeroName>
+      <P.HeroLevel>level 27</P.HeroLevel>
+      <P.HorizontalLine />
+      <P.HeroPortrait />
+      {/* <SkillEmblement/> */}
+    </P.Card>
+  );
 };
 
 export default UpgradeCard;
--- src/components/generics/UpgradeCard/parts.ts
+++ src/components/generics/UpgradeCard/parts.ts
@@ -1,10 +1,40 @@
 import styled from "styled-components";
+import heroPortrait from "assets/Temporary_Assets/hero.png";
 
 export const Card = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  position: relative;
   width: 80%;
-  height: 20%;
+  height: 25%;
   background-color: orange;
+`;
+
+export const HeroName = styled.p`
+  position: absolute;
+  top: 5%;
+  left: 5%;
+  font-size: 32px;
+`;
+
+export const HeroLevel = styled(HeroName)`
+  left: 50%;
+`;
+
+export const HorizontalLine = styled.div`
+  position: absolute;
+  width: 90%;
+  border-top: solid 1px black;
+  top: 30%;
+  left: 5%;
+`;
+
+export const HeroPortrait = styled.div`
+  position: absolute;
+  top: 30%;
+  width: 40%;
+  height: 50%;
+  margin-top: 10px;
+  background-image: url(${heroPortrait});
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
 `;
