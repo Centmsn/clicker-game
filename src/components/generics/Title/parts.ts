59e21f67af5be85012575f2aabfea9481e599b33
Szadziu <mcj.mikolajczak@gmail.com>
2021-10-31 13:14:04 +0100

    feature // add font style to GlobalStyle for whole application

--- src/components/generics/Title/parts.ts
+++ src/components/generics/Title/parts.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Title = styled.header`
   color: ${({ theme }) => theme.colors.primary};
-  font-size: 38px;
+  font-size: 1.8rem;
   text-align: center;
   padding-top: 30px;
   margin-bottom: 30px;
