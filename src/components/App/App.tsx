f8a53ac1d5c6cf6c4dc33bc5dc13d3b6497e8653
Szadziu <mcj.mikolajczak@gmail.com>
2021-11-03 16:55:31 +0100

    tech // fix all reported issues again (+1 refactor)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
-import StatsSection from "components/StatsSection";
-import * as P from "./parts";
 import React from "react";
+import StatsSection from "components/StatsSection";
 import OptionsSection from "components/OptionsSection";
 import MainSection from "components/MainSection";
 import UpgradesSection from "components/UpgradesSection";
+import * as P from "./parts";
 
 const App = () => {
   return (
