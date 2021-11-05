ac8f6d05f0a8809e8126ae99cc8434c0f5e6cb64
Szadziu <mcj.mikolajczak@gmail.com>
2021-11-05 16:43:03 +0100

    add new treasure asset's file

--- src/components/MainSection/parts.ts
+++ src/components/MainSection/parts.ts
@@ -4,7 +4,9 @@ import coinsBackground from "assets/Backgrounds/coinsBackground.jpg";
 export const Wrapper = styled.div`
   grid-area: 1/3/11/11;
   background-image: url(${coinsBackground});
-  background-position: bottom;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
   border-left: solid 6px black;
   border-right: solid 6px black;
   border-bottom: solid 6px black;
--- src/components/Treasure/parts.ts
+++ src/components/Treasure/parts.ts
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 import { StyledButtonProps } from "./constants";
-import centaur from "assets/centaur.png";
+import treasure from "assets/Backgrounds/treasureChest.png";
 
 export const Button = styled.button<StyledButtonProps>`
-  height: 300px;
-  width: 300px;
-  background: url(${centaur}) no-repeat center / cover;
+  height: 15vw;
+  width: 15vw;
+  background: url(${treasure}) no-repeat center / cover;
   transition: 0.3s;
   transform: scale(${({ isClicked }) => (isClicked ? 0.85 : 1)});
 
