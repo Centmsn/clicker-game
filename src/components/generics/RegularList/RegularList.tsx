5060b6fd9374932cbe9d1c991d6634fcb7e53121
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2021-11-08 19:39:19 +0100

    remove list wrapper and parts to make it more genernic

--- src/components/generics/RegularList/RegularList.tsx
+++ src/components/generics/RegularList/RegularList.tsx
@@ -1,13 +1,12 @@
 import { RegularListProps } from "./constants";
-import * as P from "./parts";
 
 const RegularList = ({ items, itemComponent: ItemComponent, resourceName }: RegularListProps) => {
   return (
-    <P.List>
+    <>
       {items.map((item) => {
         return <ItemComponent {...{ [resourceName]: item }} />;
       })}
-    </P.List>
+    </>
   );
 };
 
