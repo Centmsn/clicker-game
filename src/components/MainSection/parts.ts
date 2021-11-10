df661e72ee6c3f6c7e38adb633cff74988e03c9d
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2021-11-10 18:54:56 +0100

    add theme color, add fadeOut utility func, add main section tabs

--- src/components/MainSection/parts.ts
+++ src/components/MainSection/parts.ts
@@ -2,14 +2,13 @@ import styled from "styled-components";
 import coinsBackground from "assets/Backgrounds/coinsBackground.jpg";
 
 export const Wrapper = styled.div`
+  position: relative;
   grid-area: 1/3/11/11;
-
-  border-left: solid 6px black;
-  border-right: solid 6px black;
-  border-bottom: solid 6px black;
 
   background: center / cover no-repeat url(${coinsBackground});
   background-position: bottom;
+
+  overflow: hidden;
 `;
 
 export const ButtonWrapper = styled.div`
