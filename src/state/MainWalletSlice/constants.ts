39ef131600d01d563e55779aac66faa07ff9978c
Szadziu <73105872+Szadziu@users.noreply.github.com>
2021-11-12 22:02:58 +0100

    feature // added new statistics (gold per click) (#30)

--- src/state/MainWalletSlice/constants.ts
+++ src/state/MainWalletSlice/constants.ts
@@ -1,14 +1,17 @@
 export const INITIAL_WALLET_VALUE = 0;
 export const INITIAL_INCREMENT = 0;
+export const INITIAL_GOLD_PER_CLICK = 1;
 export const MIN_WALLET_VALUE = 0;
 export const MIN_INCREMENT = 0;
 
 export interface WalletState {
   value: number;
   incrementPerSecond: number;
+  goldPerClick: number;
 }
 
 export const initialState: WalletState = {
   value: INITIAL_WALLET_VALUE,
   incrementPerSecond: INITIAL_INCREMENT,
+  goldPerClick: INITIAL_GOLD_PER_CLICK,
 };
