aaa95883533c8ca1dcf47c19f5c6df433bd28728
Szadziu <73105872+Szadziu@users.noreply.github.com>
2021-11-13 18:47:07 +0100

    Modal component (#10)
    * feature // add Modal Component initial structure
    
    * feature // add modal display functionality and his style
    
    * tech // fix all reported issues
    
    * tech // fixed reported issues
    
    Co-authored-by: Wojciech Rygorowicz <wojciech.rygorowicz@gmail.com>

---
+++ src/components/generics/Modal/parts.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 100vw;
+  height: 100vh;
+
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export const ModalWindow = styled.div`
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+
+  display: flex;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  width: 30vw;
+  height: 30vh;
+
+  background-color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const CloseButton = styled.span`
+  position: absolute;
+  top: 0;
+  right: 0;
+
+  font-size: 2rem;
+
+  transition: 0.3s;
+
+  &:hover {
+    transform: scale(1.1);
+
+    cursor: pointer;
+  }
+`;
