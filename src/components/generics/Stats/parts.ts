d2d568fa09e36d8259796e3447244f678f741276
Szadziu <mcj.mikolajczak@gmail.com>
2021-11-18 21:45:26 +0100

    tech // refactor Title component styles and fix reported issues

--- src/components/generics/Stats/parts.ts
+++ src/components/generics/Stats/parts.ts
@@ -6,13 +6,8 @@ export const Stat = styled.div`
   align-items: center;
 
   width: 90%;
-  /* height: 5%; */
   margin: 10px 0;
-  /* padding: 0 10px; */
-  /* border: solid 2px ${({ theme }) => theme.colors.black}; */
 
   font-size: 0.5rem;
   color: ${({ theme }) => theme.colors.primary};
-
-  /* background-color: ${({ theme }) => theme.colors.primary}; */
 `;
