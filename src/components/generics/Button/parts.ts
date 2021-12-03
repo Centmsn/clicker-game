3414f651eb78245803499be626fa8a386e554e5a
Maciej Mikołajczak <73105872+Szadziu@users.noreply.github.com>
2021-12-03 21:44:28 +0100

    Update button component (#34)
    * feature // added variants to buttonComponent
    
    * feature // added variants to buttons, styles.
    
    * tech // added enum with button variants
    
    * fix some issues
    
    * feature // changed is pass arguments to variant to the no-string version
    
    * tech // some refactor and added variant to size button
    
    Co-authored-by: Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>

--- src/components/generics/Button/parts.ts
+++ src/components/generics/Button/parts.ts
@@ -1,13 +1,35 @@
 import styled from "styled-components";
-import { StyledButtonProps } from "./constants";
+import { ButtonVariants, Sizes, StyledButtonProps, Variants } from "./constants";
+
+const getBtnVariant = (variant: string | undefined) => {
+  switch (variant) {
+    case "PRIMARY":
+      return Variants.primary;
+    case "SECONDARY":
+      return Variants.secondary;
+    default:
+      return;
+  }
+};
+
+const getBtnSize = (size: string | undefined) => {
+  switch (size) {
+    case "SMALL":
+      return Sizes.small;
+    case "LARGE":
+      return Sizes.large;
+    default:
+      return;
+  }
+};
 
 export const Button = styled.button<StyledButtonProps>`
+  width: ${({ size }) => getBtnSize(size)};
   height: 50px;
-
-  background-color: ${({ theme }) => theme.colors.secondary};
 
   font-size: 0.7rem;
   color: ${({ theme }) => theme.colors.black};
+  ${({ variant }) => getBtnVariant(variant)}
 
   transition: 0.3s;
   pointer-events: ${({ isDisabled }) => (isDisabled ? "none" : "auto")};
@@ -15,6 +37,7 @@ export const Button = styled.button<StyledButtonProps>`
   filter: grayscale(${({ isDisabled }) => (isDisabled ? 1 : 0)});
 
   &:hover {
-    background-color: ${({ theme }) => theme.colors.primary};
+    background-color: ${({ theme, variant }) =>
+      variant === ButtonVariants.PRIMARY ? theme.colors.secondary : theme.colors.primary};
   }
 `;
--- src/components/generics/UpgradeCard/CardOverlay/CardOverlay.tsx
+++ src/components/generics/UpgradeCard/CardOverlay/CardOverlay.tsx
@@ -1,12 +1,18 @@
 import Button from "components/generics/Button";
 import { CardOverlayProps } from "./constants";
+import { ButtonVariants, ButtonSizes } from "components/generics/Button/constants";
 import coin from "assets/Additional_Assets/coinSpin.gif";
 import * as P from "./parts";
 
 const CardOverlay = ({ heroPrice, isBuyButtonDisabled, buyHero }: CardOverlayProps): JSX.Element => {
   return (
     <P.CardOverlay>
-      <Button onClick={buyHero} isDisabled={isBuyButtonDisabled}>
+      <Button
+        variant={ButtonVariants.SECONDARY}
+        size={ButtonSizes.LARGE}
+        onClick={buyHero}
+        isDisabled={isBuyButtonDisabled}
+      >
         Buy hero for {heroPrice} <img src={coin} alt="" />
       </Button>
     </P.CardOverlay>
